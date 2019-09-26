1d6131e4e23bb8011db2c5674fdbe4551a7b12df
A'Jahn Kimble <ajahnkimble@gmail.com>
2019-09-26 12:36:27 -0400

    Added alpanumeric character system

---
+++ src/components/Board.vue
@@ -0,0 +1,78 @@
+<template>
+    <div id="board">
+        
+        <square
+        v-for = "block in squares"
+        :id = "block.id"
+        :key = "block.id"
+        :style = "block.squareStyle">
+        </square>
+        
+    </div>
+</template>
+
+<script>
+import square from "./square";
+
+           
+        const boardWidth = 8;
+        const boardHeight = 8;
+        const boardArea = boardWidth * boardHeight;
+
+export default {
+    name: "Board",
+    components: {
+        square,
+    },
+    data() {
+        return {
+            squares: [],
+            row: 0,
+            columnArray: ['a','b', 'c', 'd','e','f', 'g', 'h'],
+        }
+    },
+    methods: {
+        
+        makeIds(index) {
+            console.log("makeIds")
+            //get letter from array and repeat for each row
+            let column = this.columnArray[index % boardWidth];
+            //increment row every time a new row starts
+            if (index % boardWidth === 0){
+                this.row += 1;
+            }
+            
+            return `square-${column}${this.row}` 
+        },
+        makeSquareStyle() {
+            
+        },
+        alternateColor() {
+
+        },
+        repeatSquare() {
+            console.log('repeatSquare');
+            for (let i = 0; i <= boardArea; i++) {
+                this.squares.push(               
+                    {
+                        id: this.makeIds(i),
+                        squareStyle:{
+                        width: "50px",
+                        height: "50px",
+                        backgroundColor: "#000",    
+                        }
+
+                });  
+            }   
+        }
+    },
+    created(){
+        
+       this.repeatSquare(boardArea);
+
+
+    }
+
+
+}
+</script>
---
+++ src/components/square.vue
@@ -0,0 +1,15 @@
+<template>
+    <div id="square">
+        </div>
+</template>
+
+
+<script>
+export default {
+    name: "square",
+    props: {
+        block: Object,
+    },
+    
+}
+</script>
